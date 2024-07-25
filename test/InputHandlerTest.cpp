5ecc47d48663835157e27bef12fd6cfca5d7f703
iqiac <iqiacs@gmail.com>
2024-07-25 19:01:00 +0000

    cppcoreguidelines

---
+++ test/InputHandlerTest.cpp
@@ -0,0 +1,12 @@
+#include "InputHandler.h"
+
+#include "Mocks/CommandMock.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+class InputHandlerTest : public Test {
+protected:
+};
